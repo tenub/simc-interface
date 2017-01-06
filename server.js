a1698bce3bfc419b2038b57cfa3d8a3ac71b2513
Joseph Chrzan <jchrzan1@gmail.com>
2017-01-06 16:41:42 -0500

    remove bin; clean-up

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import simc from './lib/spawn';
 
 const app = express();
 
-app.get('/', (req, res) => {
-	const options = Object.assign({}, {
-		armory: 'us,burning%20legion,jelq',
-		save: 'profiles/jelq.simc',
-		/*json: 'jelq.json',
-		max_time: 300,
-		report_details: 0,
-		ilevel_raid_report: 1,
-		iterations: 1000,
-		target_error: 0.2*/
-	});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-	simc(options, (err, data) => (
-		err ? console.error(err) : res.set('Content-Type', 'text/plain').send(data)
+app.post('/simc/profile', (req, res) => {
+	/**
+	 * {
+	 *   json: '${options.charName}.json',
+	 *   max_time: 300,
+	 *   report_details: 0,
+	 *   ilevel_raid_report: 1,
+	 *   iterations: 1000,
+	 *   target_error: 0.2
+	 * 	}
+	 */
+	const options = req.body;
+
+	if (!options.region || !options.realm || !options.charName) {
+		return res.status(400).json('Required parameters not present');
+	}
+
+	return simc(options, (err, data) => (
+		err ? res.status(500).json(err) : res.set('Content-Type', 'text/plain').send(data)
 	));
 });
 
